bdccf832d829c1573e880fccd733d7d731e73d3f
Sultan Room <sultanroomcode@gmail.com>
2024-07-14 00:51:46 +0700

    add migration to video table

---
+++ prisma/migrations/20240713174600_init/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Video` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `Video`;
+
+-- CreateTable
+CREATE TABLE `video` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `youtubeUrl` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `video_youtubeUrl_key`(`youtubeUrl`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
