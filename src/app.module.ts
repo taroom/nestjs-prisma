8fec1cff402a928766f9bb61f72db2403ea277d6
Sultan Room <sultanroomcode@gmail.com>
2024-07-14 11:57:08 +0700

    add videos module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { VideosModule } from './videos/videos.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, VideosModule],
   controllers: [AppController],
   providers: [AppService],
 })
